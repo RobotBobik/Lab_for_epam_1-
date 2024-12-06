e0fb7e6ad1e94cccfcc5b5b620764c3a140c798c
Max <0664263601a@gmail.com>
2024-12-07 00:44:05 +0200

    triger for pipeline

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const express = require('express');
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-// Ap
+// App
 const app = express();
-app.get('/', (req, res) => { 
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
